f6fd4004dafda15f24c36e96bf9330a0a24d489b
LeeWannacott <wannacottL@gmail.com>
2020-11-08 03:35:09 +1300

    Updates

---
+++ precache-manifest.dde7f140b3ce5fc8c5bc660c7aacb465.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8f06e88f59d21235fd0a8571f81d08f",
+    "url": "/table-sort-js/index.html"
+  },
+  {
+    "revision": "4b3a1fa7c290789dc8a9",
+    "url": "/table-sort-js/static/css/main.1d7b1d37.chunk.css"
+  },
+  {
+    "revision": "b52b932aa8d70d4a4bc4",
+    "url": "/table-sort-js/static/js/2.487f5125.chunk.js"
+  },
+  {
+    "revision": "eceb3578d4ecd03c45446d0941802b40",
+    "url": "/table-sort-js/static/js/2.487f5125.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b3a1fa7c290789dc8a9",
+    "url": "/table-sort-js/static/js/main.f4b1f35b.chunk.js"
+  },
+  {
+    "revision": "964ff54b6feff3ecf299",
+    "url": "/table-sort-js/static/js/runtime-main.162bbfd1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/table-sort-js/static/media/logo.5d5d9eef.svg"
+  }
+]);
