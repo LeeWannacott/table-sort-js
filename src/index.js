03e6734fbe2afe7cad67a06650859f63c954865a
LeeWannacott <wannacottL@gmail.com>
2020-11-11 02:29:15 +1300

    remove data from head

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './table-sort';
-import * as serviceWorker from './serviceWorker';
 import TestTable from './TestTable'
 import DisplayTableSort from './TableSortScript';
 import { HashRouter, BrowserRouter ,Route } from "react-router-dom";
@@ -18,16 +17,16 @@ ReactDOM.render(
 <DisplayTableSort/>
 </Route>
 </HashRouter>,
-  document.getElementById('root')
+  document.getElementById('display')
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
 
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
