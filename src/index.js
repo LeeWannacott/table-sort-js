d56e28308529f722b28a9576d60a0030387364f5
LeeWannacott <wannacottL@gmail.com>
2020-11-11 01:23:26 +1300

    Try Fragments

--- src/index.js
+++ src/index.js
@@ -20,17 +20,17 @@ ReactDOM.render(
 <DisplayTableSort/>
 </React.Fragment>
 </Route>
-</HashRouter>)
-//   document.getElementById('root')
-// );
+</HashRouter>,
+  document.getElementById('root')
+);
 
-// // If you want your app to work offline and load faster, you can change
-// // unregister() to register() below. Note this comes with some pitfalls.
-// // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
 
 
-// // If you want your app to work offline and load faster, you can change
-// // unregister() to register() below. Note this comes with some pitfalls.
-// // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
