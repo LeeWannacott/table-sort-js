1e31e6d8999c5eb448ec8be57b005de802bf87c2
Lee Wannacott <wannacottL@gmail.com>
2021-04-15 09:30:39 +1200

    Updates

--- table-sort.js
+++ table-sort.js
@@ -6,19 +6,17 @@ Licence: MIT License Copyright (c) 2021 Lee Wannacott
 GitHub Repository: https://github.com/LeeWannacott/table-sort-js
 npm package: https://www.npmjs.com/package/table-sort-js
 Demo: https://leewannacott.github.io/Portfolio/#/GitHub
-
 Install:
 Frontend: <script src="https://leewannacott.github.io/table-sort-js/table-sort.js"></script> or
 Download this file and add <script src="table-sort.js"></script> to your HTML 
-
 Backend: npm install table-sort-js and use require("../node_modules/table-sort-js/table-sort.js") 
-
 Instructions:
   Add class="table-sort" to tables you'd like to make sortable
   Click on the table headers to sort them.
 */
 
 function tableSortJs() {
+  const columnData = [];
   const columnIndexAndTableRow = {};
 
   for (let table of document.getElementsByTagName("table")) {
@@ -36,28 +34,18 @@ function tableSortJs() {
 
     const tableHead = sortableTable.querySelector("thead");
     const tableBody = sortableTable.querySelector("tbody");
-    const tableRows = tableBody.querySelectorAll("tr");
     const tableHeadHeaders = tableHead.querySelectorAll("th");
 
     tableHead.style.cursor = "pointer";
 
-    for (let [columnIndex, th] of tableHeadHeaders.entries()) {
+    for (let [columnIndex, th] of tableHeadHeaders.entries("table")) {
       let timesClickedColumn = 0;
 
       th.addEventListener("click", function () {
-        const columnData = [];
         timesClickedColumn += 1;
 
-        getTableData();
-        updateTable();
-
-        function updateTable() {
-          for (let [i, tr] of tableRows.entries()) {
-            tr.innerHTML = columnIndexAndTableRow[columnData[i]];
-          }
-        }
-
-        function getTableData() {
+        function getTableDataOnClick() {
+          const tableRows = tableBody.querySelectorAll("tr");
           for (let [i, tr] of tableRows.entries()) {
             let tdInnerHTML = tr.querySelectorAll('td').item(columnIndex).innerHTML;
             if (tdInnerHTML.trim() !== "") {
@@ -69,7 +57,6 @@ function tableSortJs() {
               columnIndexAndTableRow["!X!Y!Z!#" + i] = tr.innerHTML;
             }
           }
-
           function naturalSortAescending(a, b) {
             if (a.includes("X!Y!Z!#")) {
               return 1;
@@ -83,30 +70,30 @@ function tableSortJs() {
               );
             }
           }
-
           function naturalSortDescending(a, b) {
             return naturalSortAescending(b, a);
           }
 
           function clearArrows(arrowUp = "▲", arrowDown = "▼") {
-            th.innerText = th.innerText.replace(arrowUp, "");
-            th.innerText = th.innerText.replace(arrowDown, "");
+            let ifDownArrow = th.innerText.includes(arrowDown);
+            let ifUpArrow = th.innerText.includes(arrowUp);
+            if (ifDownArrow === true || ifUpArrow === true) {
+              th.innerText = th.innerText.replace(arrowUp, "");
+              th.innerText = th.innerText.replace(arrowDown, "");
+            }
           }
 
           let arrowUp = " ▲";
           let arrowDown = " ▼";
 
           // Sort naturally; default aescending unless th contains 'order-by-desc' as className.
-          if (columnData[0] === undefined) {
-            return;
-          }
-
-          let desc = th.classList.contains('order-by-desc');
-          let tableArrows = sortableTable.classList.contains('table-arrows');
-
-          if (timesClickedColumn === 1) {
-            if (desc) {
-              if (tableArrows) {
+          if (typeof columnData[0] !== "undefined") {
+            originalColumnText = th.innerText;
+            if (
+              th.classList.contains("order-by-desc") &&
+              timesClickedColumn === 1
+            ) {
+              if (sortableTable.classList.contains("table-arrows")) {
                 clearArrows(arrowUp, arrowDown);
                 th.insertAdjacentText("beforeend", arrowDown);
               }
@@ -114,17 +101,11 @@ function tableSortJs() {
                 numeric: true,
                 ignorePunctuation: true,
               });
-            } else {
-              if (tableArrows) {
-                clearArrows(arrowUp, arrowDown);
-                th.insertAdjacentText("beforeend", arrowUp);
-              }
-              columnData.sort(naturalSortAescending);
-            }
-          } else if (timesClickedColumn === 2) {
-            timesClickedColumn = 0;
-            if (desc) {
-              if (tableArrows) {
+            } else if (
+              th.classList.contains("order-by-desc") &&
+              timesClickedColumn === 2
+            ) {
+              if (sortableTable.classList.contains("table-arrows")) {
                 clearArrows(arrowUp, arrowDown);
                 th.insertAdjacentText("beforeend", arrowUp);
               }
@@ -132,15 +113,33 @@ function tableSortJs() {
                 numeric: true,
                 ignorePunctuation: true,
               });
-            } else {
-              if (tableArrows) {
+              timesClickedColumn = 0;
+            } else if (timesClickedColumn === 1) {
+              if (sortableTable.classList.contains("table-arrows")) {
+                clearArrows(arrowUp, arrowDown);
+                th.insertAdjacentText("beforeend", arrowUp);
+              }
+              columnData.sort(naturalSortAescending);
+            } else if (timesClickedColumn === 2) {
+              if (sortableTable.classList.contains("table-arrows")) {
                 clearArrows(arrowUp, arrowDown);
                 th.insertAdjacentText("beforeend", arrowDown);
               }
               columnData.sort(naturalSortDescending);
+              timesClickedColumn = 0;
             }
           }
         }
+        getTableDataOnClick();
+
+        function returnSortedTable() {
+          const tableRows = tableBody.querySelectorAll("tr");
+          for (let [i, tr] of tableRows.entries()) {
+            tr.innerHTML = columnIndexAndTableRow[columnData[i]];
+          }
+          columnData.length = 0;
+        }
+        returnSortedTable();
       });
     }
   }
