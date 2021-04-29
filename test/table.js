e0211e8195b77f12c6467bef070e0cfd0cd3b19f
Lee Wannacott <wannacottL@gmail.com>
2021-04-29 21:36:10 +1200

    Made test table dynamically size based on the length of the TestTableData array and passed class tags as a parameter for createTestTable

--- test/table.js
+++ test/table.js
@@ -1,60 +1,49 @@
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
-require('iconv-lite').encodingExists('foo')
-tableSortJs = require('../public/table-sort')
+require("iconv-lite").encodingExists("foo");
+tableSortJs = require("../public/table-sort");
 
-function createTestTable(testTableData){
-  let classes = "";
-  if(testTableData.includes('file-size')){
-    classes = "file-size"
+function createTestTable(testTableData, classTags="") {
+
+  let getClassTagsForTH = [];
+  let testTableThRow = `<tr><th class="${classTags}">Testing Column</th></tr>`;
+  getClassTagsForTH.push(testTableThRow);
+
+  let testTableTdRows = [];
+  for (let i = 0; i < testTableData.length; i++) {
+    let testTableTdRow = `<tr><td>${testTableData[i]}</td></tr>`;
+    testTableTdRows.push(testTableTdRow);
   }
 
   const dom = new JSDOM(`<!DOCTYPE html>
   <html>
-  <head>
-  </head>
-  <body>
-  <table class="table-sort">
-  <thead>
-    <tr>
-      <th class="${classes}">Testing Column</th>
-    </tr>
-    </thead>
+    <head>
+    </head>
+    <body>
+      <table class="table-sort">
+      <thead>
+      ${getClassTagsForTH}
+      </thead>
     <tbody>
-    <tr>
-      <td>${testTableData[0]}</td>
-    </tr>
-    <tr>
-      <td>${testTableData[1]}</td>
-    </tr>
-    <tr>
-      <td>${testTableData[2]}</td>
-    </tr>
-    <tr>
-      <td>${testTableData[3]}</td>
-    </tr>
-    <tr>
-      <td>${testTableData[4]}</td>
-    </tr>
+    ${testTableTdRows}
     </tbody>
   </table> 
   </body>
   </html>`);
-  // The script will be executed and modify the DOM:
-  dom.window.document.body.children.length === 2;
 
   // Call tablesort and make table sortable and simulate click from a user.
-  tableSortJs(dom.window.document)
-  dom.window.document.querySelector("table th").click()
+  tableSortJs(dom.window.document);
+  dom.window.document.querySelector("table th").click();
 
   // Make an array from table contents to test if sorted correctly.
-  let table = dom.window.document.querySelector("table")
+  let table = dom.window.document.querySelector("table");
   const tableBody = table.querySelector("tbody");
   const tableRows = tableBody.querySelectorAll("tr");
-  testIfSortedList = []
+  testIfSortedList = [];
   for (let [i, tr] of tableRows.entries()) {
-    testIfSortedList.push(tr.querySelectorAll('td').item(0).innerHTML)
-    }
-    return testIfSortedList
+    testIfSortedList.push(tr.querySelectorAll("td").item(0).innerHTML);
+  }
+  return testIfSortedList;
 }
+
 module.exports = createTestTable;
--- test/table.test.js
+++ test/table.test.js
@@ -3,108 +3,107 @@ const createTestTable = require("./table");
 // toBe for primitives like strings, numbers or booleans for everything else use toEqual(object)
 
 test("Alpha - Capitalized ", () => {
-   expect(createTestTable(["Echo", "Alpha", "Bravo", "Charlie", "Delta"])).toStrictEqual([
-      "Alpha",
-      "Bravo",
-      "Charlie",
-      "Delta",
-      "Echo",
-   ]);
+  expect(
+    createTestTable(["Echo", "Alpha", "Bravo", "Charlie", "Delta"])
+  ).toStrictEqual(["Alpha", "Bravo", "Charlie", "Delta", "Echo"]);
 });
 
 test("Alpha - Lowercase", () => {
-   expect(createTestTable(["echo", "alpha", "bravo", "charlie", "delta"])).toStrictEqual([
-      "alpha",
-      "bravo",
-      "charlie",
-      "delta",
-      "echo",
-   ]);
+  expect(
+    createTestTable(["echo", "alpha", "bravo", "charlie", "delta"])
+  ).toStrictEqual(["alpha", "bravo", "charlie", "delta", "echo"]);
 });
 
 test("Numerical", () => {
-   expect(createTestTable([5, 3, 4, 1, 2])).toStrictEqual(["1", "2", "3", "4", "5"]);
+  expect(createTestTable([5, 3, 4, 1, 2])).toStrictEqual([
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+  ]);
 });
 
 test("Alphanumeric", () => {
-   expect(createTestTable(["Alpha1", "Echo5", "Bravo2", "Charlie3", "Delta4"])).toStrictEqual([
-      "Alpha1",
-      "Bravo2",
-      "Charlie3",
-      "Delta4",
-      "Echo5",
-   ]);
+  expect(
+    createTestTable(["Alpha1", "Echo5", "Bravo2", "Charlie3", "Delta4"])
+  ).toStrictEqual(["Alpha1", "Bravo2", "Charlie3", "Delta4", "Echo5"]);
 });
 
 test("Dates", () => {
-   expect(
-      createTestTable(["1979/9/6", "2008/4/9", "1879/12/16", "1978/4/6", "1978/4/16"])
-   ).toStrictEqual(["1879/12/16", "1978/4/6", "1978/4/16", "1979/9/6", "2008/4/9"]);
+  expect(
+    createTestTable([
+      "1979/9/6",
+      "2008/4/9",
+      "1879/12/16",
+      "1978/4/6",
+      "1978/4/16",
+    ])
+  ).toStrictEqual([
+    "1879/12/16",
+    "1978/4/6",
+    "1978/4/16",
+    "1979/9/6",
+    "2008/4/9",
+  ]);
 });
 
 test("Money", () => {
-   expect(createTestTable(["$29", "$93", "$84", "$20", "$58"])).toStrictEqual([
-      "$20",
-      "$29",
-      "$58",
-      "$84",
-      "$93",
-   ]);
+  expect(createTestTable(["$29", "$93", "$84", "$20", "$58"])).toStrictEqual([
+    "$20",
+    "$29",
+    "$58",
+    "$84",
+    "$93",
+  ]);
 });
 
 test("Empty cells sort at the end.", () => {
-   expect(createTestTable(["Echo", "", "Bravo", "", "Alpha"])).toStrictEqual([
-      "Alpha",
-      "Bravo",
-      "Echo",
-      "",
-      "",
-   ]);
+  expect(createTestTable(["Echo", "", "Bravo", "", "Alpha"])).toStrictEqual([
+    "Alpha",
+    "Bravo",
+    "Echo",
+    "",
+    "",
+  ]);
 });
 
 // Disabled while implementing file-size
-// test("file-size", () => {
-//    expect(createTestTable(["20KB", "20GB", "20TB", "20Kib", "20MiB", "file-size"])).toStrictEqual([
-//       "19.53KiB",
-//       "20.00KiB",
-//       "20.00MiB",
-//       "18.62GiB",
-//       "18.19TiB",
-//    ]);
-// });
+test("file-size", () => {
+  expect(
+    createTestTable(["20KB", "20GB", "20TB", "20Kib", "20MiB"],"file-size")
+  ).toStrictEqual([
+    "19.53 KiB",
+    "20.00 KiB",
+    "20.00 MiB",
+    "18.62 GiB",
+    "18.18 TiB",
+  ])
+});
 
 //New tests
 
 test("Alpha - lower & upper", () => {
-   expect(createTestTable(["AlPhA", "bRaVo", "EcHo", "ChArLiE", "dElTa"])).toStrictEqual([
-      "AlPhA",
-      "bRaVo",
-      "ChArLiE",
-      "dElTa",
-      "EcHo",
-   ]);
+  expect(
+    createTestTable(["AlPhA", "bRaVo", "EcHo", "ChArLiE", "dElTa"])
+  ).toStrictEqual(["AlPhA", "bRaVo", "ChArLiE", "dElTa", "EcHo"]);
 });
 
 test("Floating point numbers", () => {
-   expect(createTestTable([6.23, 0.25, 3.15, 9.09, 0.35])).toStrictEqual([
-      "0.25",
-      "0.35",
-      "3.15",
-      "6.23",
-      "9.09",
-   ]);
+  expect(createTestTable([6.23, 0.25, 3.15, 9.09, 0.35])).toStrictEqual([
+    "0.25",
+    "0.35",
+    "3.15",
+    "6.23",
+    "9.09",
+  ]);
 });
 
 test("Release Versions", () => {
-   expect(createTestTable(["4.0.1", "3.0.2", "4.1.0", "3.0.4", "4.2.0"])).toStrictEqual([
-      "3.0.2",
-      "3.0.4",
-      "4.0.1",
-      "4.1.0",
-      "4.2.0",
-   ]);
+  expect(
+    createTestTable(["4.0.1", "3.0.2", "4.1.0", "3.0.4", "4.2.0"])
+  ).toStrictEqual(["3.0.2", "3.0.4", "4.0.1", "4.1.0", "4.2.0"]);
 });
-
 
 // Tests For Sorting not yet implemented
 // test("Sizes", () => {
