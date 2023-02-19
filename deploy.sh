d95b9ba7ca031ccfb3e6976c2c35cc09fb7cc074
LeeWannacott <WannacottL@gmail.com>
2023-02-19 20:52:22 +1300

    Add linting to deploy script.

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,5 @@ cp public/table-sort.js npm/table-sort.js
 cp README.md npm/README.md
 cp LICENSE npm/LICENSE
 cp Contributors.md npm/Contributors.md
+npx prettier --write .
 echo "Reminder: Update npm package to new version in npm/package.json"
