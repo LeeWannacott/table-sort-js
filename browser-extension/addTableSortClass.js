2ac6c0009a8ceaaa217c9d8ae36e32e8e44e49f2
LeeWannacott <WannacottL@gmail.com>
2023-05-09 23:22:02 +1200

    Update browser extension.

--- browser-extension/addTableSortClass.js
+++ browser-extension/addTableSortClass.js
@@ -1,6 +1,4 @@
-document.body.style.border = "1px solid red";
-
-let tables = document.querySelectorAll("table");
+const tables = document.querySelectorAll("table");
 
 Array.from(tables).map((table) => {
   return table.classList.add("table-sort");
