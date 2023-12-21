313e6a8104ef0816449f9d7c570257e499206b63
Ali Jahanshiri <alijsh@gmail.com>
2023-12-21 12:42:23 +0330

    Update table-sort.js

--- public/table-sort.js
+++ public/table-sort.js
@@ -26,7 +26,7 @@ function tableSortJs(testingTableSortJS = false, domDocumentWindow = document) {
   const [getTagTable] = getHTMLTables();
   const columnIndexAndTableRow = {};
   for (let table of getTagTable) {
-    if (table.classList.contains("table-sort")) {
+    if (table.classList.contains("table-sort") && !table.classList.contains("table-processed")) {
       makeTableSortable(table);
     }
   }
@@ -113,12 +113,8 @@ function tableSortJs(testingTableSortJS = false, domDocumentWindow = document) {
     }
   }
 
-  function makeTableSortable(sortableTable) {
-    if (sortableTable.classList.contains("table-processed")) {
-        return;
-    } else {
-        sortableTable.classList.add("table-processed");
-    }
+  function makeTableSortable(sortableTable) {  
+    sortableTable.classList.add("table-processed");
     const table = {
       bodies: getTableBodies(sortableTable),
       theads: sortableTable.querySelectorAll("thead"),
